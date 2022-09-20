b70219a87e94db1a8218bd8945350fe8b8e868b2
Alexandr <Shurik251083@gmail.com>
2022-09-20 10:59:08 +0400

    Создали цикл соизмеримый длине массива

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[]array1 = new string[5] {"123", "23", "hello", "word", "res"};
+string[]array2 = new string[array1.Length];
+
+void SecondArrayWinthlF(string[]array1, string[array2])
+{
+int count = 0;
+for (int = 0, i < array1.Length, int++)
+
+
+}
