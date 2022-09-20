d0f9cdf26b13ecff0d121b704de89b5f2f244a2a
Alexandr <Shurik251083@gmail.com>
2022-09-20 11:25:21 +0400

    Проверка условия (<= 3).

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿string[]array1 = new string[5] {"123", "23", "hello", "word", "res"};
-string[]array2 = new string[array1.Length];
+﻿string[] array1 = new string[5] { "123", "23", "hello", "word", "res" };
+string[] array2 = new string[array1.Length];
 
-void SecondArrayWinthlF(string[]array1, string[array2])
+void SecondArrayWinthlF(string[] array1, string[array2])
 {
-int count = 0;
-for (int = 0, i < array1.Length, int++)
-
-
+    int count = 0;
+    for (int = 0, i < array1.Length, i++)
+    {
+        if (array1[1].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
 }
+
